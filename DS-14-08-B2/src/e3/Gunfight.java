95c4d4713179612751d52e167c54c90e02b4444a
diego <diego.saavedra1@udc.es>
2020-11-27 22:57:55 +0100

    Duel del 3.

--- DS-14-08-B2/src/e3/Gunfight.java
+++ DS-14-08-B2/src/e3/Gunfight.java
@@ -1,38 +1,36 @@
 package e3;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Gunfight
 {
-	private List<GunslingerAction> g1Acts = new ArrayList<>(), g2Acts = new ArrayList<>();
-
+	//gunslinger1.setBehavior(new Behavior)
 	public void duel(Gunslinger g1, Gunslinger g2)
 	{
 		GunslingerAction g1Act = g1.action(), g2Act = g1.action();
-		Gunslinger ganador = null;
+		String ganador = "";
+		int Round=1;
 		do
 		{
+			System.out.print("Round "+Round+"-------------------------\n");
 			g1.rivalAction(g2Act);
 			g2.rivalAction(g1Act);
 
 			g1Act = g1.action();
 			g2Act = g2.action();
-			g1Acts.add(g1Act);
-			g2Acts.add(g2Act);
-
+			System.out.print("Gunslinger 1: "+g1Act+"\nGunslinger 2: "+g2Act+"\n");
 			if((g1Act == GunslingerAction.MACHINE_GUN) ||
-							((g1Act == GunslingerAction.SHOOT) && (g2Act == GunslingerAction.RELOAD)))
+					((g1Act == GunslingerAction.SHOOT) && (g2Act == GunslingerAction.RELOAD)))
 			{
-				ganador = g1;
+				ganador = "Winner: GUNSLINGER1";
 			}
-			else if((g2Act == GunslingerAction.MACHINE_GUN) ||
-							((g2Act == GunslingerAction.SHOOT) && (g1Act == GunslingerAction.RELOAD)))
+			if((g2Act == GunslingerAction.MACHINE_GUN) ||
+					((g2Act == GunslingerAction.SHOOT) && (g1Act == GunslingerAction.RELOAD)))
 			{
-				ganador = g1;
+				if(ganador.equals("Winner: GUNSLINGER1"))ganador = "Tie";
+				else ganador = "Winner: GUNSLINGER2";
 			}
-		}while(ganador == null);
+			if(ganador.equals(""))System.out.print("The duel continues ...\n");
 
+		}while(ganador.equals(""));
 	}
 }
 
